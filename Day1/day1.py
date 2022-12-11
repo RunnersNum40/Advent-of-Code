81843fb57bf0f8615fc0c77fa222abad1ee71100
Ted Pinkerton <ted.pinkerton@mail.utoronto.ca>
2022-12-11 16:43:46 -0500

    Completed day 1

---
+++ Day1/day1.py
@@ -0,0 +1,18 @@
+# day1.py
+from heapq import nlargest
+
+file_name = "input.txt"
+with open(file_name, "r") as file:
+    data = [line for line in file]
+
+elves = [[]]
+for line in data:
+    if line == "\n":
+        elves.append([])
+    else:
+        elves[-1].append(int(line))
+
+elves = list(map(sum, elves))
+
+print(max(elves))
+print(sum(nlargest(3, elves)))
