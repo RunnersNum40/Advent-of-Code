3a34867bb88ebb0795af4bc513acb99a38d3c6b8
Ted Pinkerton <ted.pinkerton@mail.utoronto.ca>
2022-12-12 19:13:29 -0500

    cleaned up day5.py

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -7,22 +7,35 @@ file_name = "input.txt"
 with open(file_name, "r") as file:
     data = [line for line in file]
 
-piles_end = data.index("\n")
+def read(data: list) -> tuple:
+    """Read the piles and steps from the contents of a file.
+    
+    Args:
+        data (list): List of strings of each line of the input
+    Returns:
+        [tuple, tuple]: piles, steps
+    """
+    piles_end = data.index("\n")
+    pattern = r"move (\d+) from (\d+) to (\d+)"
 
-# Find each of the piles by iterating down each column
-piles = []
-for n in range(1, len(data[0]), 4):
-    # Iterate through each row in reverse order so the top of the pile is the end
-    piles.append([row[n] for row in data[piles_end-2::-1] if row[n] != " "])
-piles = tuple(piles)
+    # Find each of the piles by iterating down each column
+    piles = []
+    for n in range(1, len(data[0]), 4):
+        # Iterate through each row in reverse order so the top of the pile is the end
+        piles.append([row[n] for row in data[piles_end-2::-1] if row[n] != " "])
+    piles = tuple(piles)
 
-# Find the move commands by extracting with regex
-pattern = r"move (\d+) from (\d+) to (\d+)"
-steps = []
-for row in data[piles_end+1:]:
-    match = re.search(pattern, row)
-    step = tuple(map(int, (match.group(1), match.group(2), match.group(3))))
-    steps.append(step)
+    # Find the move commands by extracting with regex
+    steps = []
+    for row in data[piles_end+1:]:
+        match = re.search(pattern, row)
+        step = (int(match.group(1)), 
+                int(match.group(2))-1, 
+                int(match.group(3))-1)
+        steps.append(step)
+    steps = tuple(steps)
+
+    return piles, steps
 
 
 def apply_step_reverse(piles: tuple, step: tuple) -> list:
@@ -36,7 +49,7 @@ def apply_step_reverse(piles: tuple, step: tuple) -> list:
     """
     piles = tuple(pile.copy() for pile in piles)
     for _ in range(step[0]):
-        piles[step[2]-1].append(piles[step[1]-1].pop())
+        piles[step[2]].append(piles[step[1]].pop())
 
     return piles
 
@@ -51,11 +64,13 @@ def apply_step_forward(piles: tuple, step: tuple) -> list:
         The new configuration of piles
     """
     piles = tuple(pile.copy() for pile in piles)
-    piles[step[2]-1].extend(piles[step[1]-1][-step[0]:])
-    del piles[step[1]-1][-step[0]:]
+    piles[step[2]].extend(piles[step[1]][-step[0]:])
+    del piles[step[1]][-step[0]:]
 
     return piles
 
+
+piles, steps = read(data)
 
 reverse = piles
 forward = piles
