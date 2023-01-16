fa3b25ab8ccba06000ce619406ec22ddc2eebddd
Ted Pinkerton <ted.pinkerton@mail.utoronto.ca>
2023-01-15 22:36:26 -0500

    Added comments

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -11,7 +11,19 @@ print(data)
 
 
 def invisible(line: list, tree: int) -> bool:
+    """Return if the tree is invisible from the edge.
+    
+    Check if any of the trees are >= to the original tree.
+    
+    Args:
+        line (list): height of trees
+        tree (int): height of the tree to compare to
+    
+    Returns:
+        bool: False if the tree is visible from the edge else True.
+    """
     return any(j >= tree for j in line)
+
 
 def visible(grid: list, pos: tuple) -> bool:
     """Return if the tree at a position in the grid is visible.
@@ -30,15 +42,27 @@ def visible(grid: list, pos: tuple) -> bool:
         # Generate a slice along one axis
         index = tuple([pos[j] if j != n else ... for j in range(len(pos))])
         line = grid[index]
+        # Get both directions
         dir1 = invisible(line[:i], grid[pos])
         dir2 = invisible(line[i+1:], grid[pos])
-        # Find the visibility in the positive and negative directions.
+        # Find the visibility in the positive and negative directions
         if not (dir1 and dir2):
             return True
     return False
 
 
-def view(line: list, tree: int = 0) -> int:
+def view(line: list, tree: int) -> int:
+    """Return the number of trees less than a value starting from the beginning
+    
+    Loop until a tree too tall is found or there are no trees.
+    
+    Args:
+        line (list): height of trees getting farther from the start
+        tree (int): height of the tree to compare to
+    
+    Returns:
+        int: Number of visible trees
+    """
     for n, i in enumerate(line):
         if i >= tree:
             return n+1
@@ -62,6 +86,7 @@ def scenic_score(grid: list, pos: tuple) -> int:
         # Generate a slice along one axis
         index = tuple([pos[j] if j != n else ... for j in range(len(pos))])
         line = grid[index]
+        # Find the scenic value in the positive and negative directions
         dirs.append(view(line[:i][::-1], grid[pos]))
         dirs.append(view(line[i+1:], grid[pos]))
     return np.prod(dirs)
