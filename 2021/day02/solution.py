08f720330474df6c233fd8cbcac59c8bccf8ed44
Ted <ted.pinkerton@mail.utoronto.ca>
2024-05-14 20:11:44 -0400

    clean up 2021 day 2 python

--- 2021/day02/solution.py
+++ 2021/day02/solution.py
@@ -1,36 +1,45 @@
 import numpy as np
 
+
+def parse_line(command, number):
+    return (command, int(number))
+
+
 file_name = "input.txt"
 with open(file_name, "r") as file:
-    process = lambda command, number: (command, int(number))
-    data = [process(*line.split(" ")) for line in file]
+    data = [parse_line(*line.split(" ")) for line in file]
 
-# Part 1
+
 def part1(data):
-    lookup = {"forward":np.array([1, 0]), "up":np.array([0, -1]), "down":np.array([0, 1])} # Lookup table for masks
-    pos = np.array([0, 0]) # Initial position [x, y]
+    lookup = {
+        "forward": np.array([1, 0]),
+        "up": np.array([0, -1]),
+        "down": np.array([0, 1]),
+    }
+    pos = np.array([0, 0])
 
     for command, number in data:
         # Lookup the number mask and add the scaled mask to pos
-        pos += lookup[command]*number
+        pos += lookup[command] * number
 
-    return pos[0]*pos[1]
+    return pos[0] * pos[1]
 
-# Part 2
+
 def part2(data):
-    mask = np.array([1, 0]) # Intialize the mask, [1, aim]
-    pos = np.array([0, 0]) # Initial position, [x, y]
+    mask = np.array([1, 0])
+    pos = np.array([0, 0])
 
     for command, number in data:
         if command == "forward":
             # Add to position
-            pos += mask*number
+            pos += mask * number
         else:
             # Increase or decrease aim
-            mask[1] += {"up":-1, "down":1}[command]*number
+            mask[1] += {"up": -1, "down": 1}[command] * number
 
-    return pos[0]*pos[1]
+    return pos[0] * pos[1]
 
-#Running
-print("Part 1:", part1(data)) # 1936494
-print("Part 2:", part2(data)) # 1997106066
+
+# Running
+print("Part 1:", part1(data))  # 1936494
+print("Part 2:", part2(data))  # 1997106066
