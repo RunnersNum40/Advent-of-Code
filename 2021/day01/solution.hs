b9c138a1cbfa38d6b6571673b6f5a2d4ba94eb00
Ted <ted.pinkerton@mail.utoronto.ca>
2024-05-15 15:04:37 -0400

    Update typing

--- 2021/day01/solution.hs
+++ 2021/day01/solution.hs
@@ -11,14 +11,14 @@ main = do
   let part2Result = countSlidingWindowIncreases depths
   print part2Result
 
-countIncreases :: [Int] -> Int
+countIncreases :: (Ord a) => [a] -> Int
 countIncreases (x : y : xs) = (if y > x then 1 else 0) + countIncreases (y : xs)
 countIncreases _ = 0
 
-countSlidingWindowIncreases :: [Int] -> Int
+countSlidingWindowIncreases :: (Num a, Ord a) => [a] -> Int
 countSlidingWindowIncreases depths = countIncreases (slidingWindowSums 3 depths)
 
-slidingWindowSums :: Int -> [Int] -> [Int]
+slidingWindowSums :: (Num a) => Int -> [a] -> [a]
 slidingWindowSums n xs
   | length xs < n = []
   | otherwise = sum (take n xs) : slidingWindowSums n (tail xs)
